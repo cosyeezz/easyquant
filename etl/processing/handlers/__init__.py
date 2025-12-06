30e7bec740a1cc37f91ccce20c27e52ae29f2035
dane <zhangxiaodong@89holdings.com>
2025-12-06 19:10:23 +0800

    重构

--- etl/processing/handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .save_daily_data import SaveDailyDataHandler
-from .csv_adapter import CsvAdapterHandler
-from .clean_data import CleanDataHandler
-
-__all__ = [
-    "SaveDailyDataHandler",
-    "CsvAdapterHandler",
-    "CleanDataHandler",
-]
